e6a7c2120095dc5cc62c87e8e3b10587992fe915
Nthumo <emmanuelmutinda235@gmail.com>
2024-11-21 10:31:26 +0300

    manager>accounting .2

--- frontend/malicloud/src/pages/Managers/Accounting.jsx
+++ frontend/malicloud/src/pages/Managers/Accounting.jsx
@@ -5,7 +5,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Input } from '@/components/ui/input';
-import { Search, Circle, CircleCheck, CircleChevronLeft, MoveUp, Ellipsis } from 'lucide-react';
+import { Search, Circle, CircleCheck, CircleChevronLeft, MoveUp, Ellipsis, ArrowUp } from 'lucide-react';
 import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
 import { Table, TableHeader, TableRow, TableHead, TableCell,TableBody } from '@/components/ui/table';
 import { Badge } from '@/components/ui/badge';
@@ -70,9 +70,9 @@ export default function Accounting() {
               <div>
                 <Tabs defaultValue='all' className='mt-4'>
                   <TabsList className='gap-14'>
-                    <TabsTrigger value='all'>All</TabsTrigger>
-                    <TabsTrigger value='income'>Income</TabsTrigger>
-                    <TabsTrigger value='expenses'>Expenses</TabsTrigger>
+                    <TabsTrigger value='all' className='dark:data-[state=active]:border-white data-[state=active]:border-black data-[state=active]:border-b-[4px] data-[state=active]:bg-transparent'>All</TabsTrigger>
+                    <TabsTrigger value='income' className='dark:data-[state=active]:border-white data-[state=active]:border-black  data-[state=active]:border-b-[4px] data-[state=active]:bg-transparent'>Income</TabsTrigger>
+                    <TabsTrigger value='expenses' className='dark:data-[state=active]:border-white data-[state=active]:border-black data-[state=active]:border-b-[4px] data-[state=active]:bg-transparent'>Expenses</TabsTrigger>
                   </TabsList>
                   <TabsContent value='all'>
                     <div>
@@ -233,9 +233,76 @@ export default function Accounting() {
              
             )}
             {selectedCategory === 'Balances' && (
+              <>
               <div>
-                Balances
+              <Tabs defaultValue='0-30' className='mt-4'>
+                <TabsList>
+                  <TabsTrigger value='0-30' className='data-[state=active]:border-b-[4px] dark:data-[state=active]:border-white data-[state=active]:border-black data-[state=active]:bg-transparent'>0-30 days</TabsTrigger>
+                  <TabsTrigger value='30-60' className='data-[state=active]:border-b-[4px] dark:data-[state=active]:border-white data-[state=active]:border-black data-[state=active]:bg-transparent'>30-60 days</TabsTrigger>
+                  <TabsTrigger value='60-90' className='data-[state=active]:border-b-[4px] dark:data-[state=active]:border-white data-[state=active]:border-black  data-[state=active]:bg-transparent'>60-90 days</TabsTrigger>
+                  <TabsTrigger value='all time' className='data-[state=active]:border-b-[4px] dark:data-[state=active]:border-white data-[state=active]:border-black  data-[state=active]:bg-transparent'>All time</TabsTrigger>
+                </TabsList>
+                <TabsContent value='0-30'>
+                  <Card className='mt-4 h-[400px] w-[1050px]'>
+                    <CardContent className='p-0'>
+                        <Table className=''>
+                          <TableHeader>
+                            <TableRow className='dark:bg-zinc-900 dark:hover:bg-zinc-900 bg-zinc-200 hover:bg-zinc-200'>
+                              <TableHead className='flex items-center gap-2'>Outstanding balance <MoveUp className='h-4 w-4'/></TableHead>
+                              <TableHead>Contact</TableHead>
+                              <TableHead></TableHead>
+                            </TableRow>
+                          </TableHeader>
+                          <TableRow className='hover:bg-transparent'>
+                            <TableCell>Ksh. 30000</TableCell>
+                            <TableCell>John Doe</TableCell>
+                            <TableCell className='flex gap-2'>
+                              Send notice <Ellipsis/>
+                            </TableCell>
+                          </TableRow>
+                          <TableRow className='hover:bg-transparent'>
+                            <TableCell>Ksh. 30000</TableCell>
+                            <TableCell>John Doe</TableCell>
+                            <TableCell className='flex gap-2'>
+                              Send notice <Ellipsis/>
+                            </TableCell>
+                          </TableRow>
+                          <TableRow className='hover:bg-transparent'>
+                            <TableCell>Ksh. 30000</TableCell>
+                            <TableCell>John Doe</TableCell>
+                            <TableCell className='flex gap-2'>
+                              Send notice <Ellipsis/>
+                            </TableCell>
+                          </TableRow>
+                          <TableRow className='hover:bg-transparent'>
+                            <TableCell>Ksh. 30000</TableCell>
+                            <TableCell>John Doe</TableCell>
+                            <TableCell className='flex gap-2'>
+                              Send notice <Ellipsis/>
+                            </TableCell>
+                          </TableRow>
+                        </Table>
+                    </CardContent>
+                  </Card>
+                </TabsContent>
+                <TabsContent value='30-60'>
+                  <Card className='mt-4 h-[400px] w-[1050px]'>
+                    <h1 className='text-center'>30-60 days</h1>
+                  </Card>
+                </TabsContent>
+                <TabsContent value='60-90'>
+                  <Card className='mt-4 h-[400px] w-[1050px]'>
+                  <h1 className='text-center'>60-90 days</h1>
+                  </Card>
+                </TabsContent>
+                <TabsContent value='all time'>
+                  <Card className='mt-4 h-[400px] w-[1050px]'>
+                  <h1 className='text-center'>All time</h1>
+                  </Card>
+                </TabsContent>
+              </Tabs>
               </div>
+              </>
             )}
             {selectedCategory === 'Recurring' && (
               <div>
