04cac8bf2e90e22a2d69d7ef141affd65427ed02
Nthumo <emmanuelmutinda235@gmail.com>
2024-11-28 09:29:55 +0300

    app structure .1

--- backend/malicloud/malicloud/settings.py
+++ backend/malicloud/malicloud/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'properties',
+    'leases',
+    'tenants',
+    'attachments',
+    'payments',
+    'features',
+    'reports',
 ]
 
 MIDDLEWARE = [
