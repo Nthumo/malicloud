df20fabe37dd102b5e1b486dc9415e1ee2ff9cc3
Nthumo <emmanuelmutinda235@gmail.com>
2025-01-14 16:59:06 +0300

    frontend edit .3

--- frontend/malicloud/src/pages/Tenants/Rent.jsx
+++ frontend/malicloud/src/pages/Tenants/Rent.jsx
@@ -40,12 +40,13 @@ function TenantRent() {
     <title>Accounting - Tenant</title>
   </Helmet>
     <Header/>
-    <h1 className='absolute top-32 left-36 text-2xl font-bold'>Accounting</h1>
-    <div className='relative top-12 grid gap-4'>
+    <div className='relative top- grid gap-4 mb-4'>
+      <div className='flex justify-center justify-between md:ml-20 md:mr-4 ml-16'>
+        <h1 className=' md:text-2xl font-bold'>Accounting</h1>
         <DropdownMenu className=''>
-          <DropdownMenuTrigger className='border-none absolute right-12'>
-            <button className='bg-green-600 dark:bg-green-900 hover:bg-green-500 dark:hover:bg-green-800 relative h-12 p-2 rounded-xl border-'>
-              <h1 className='text-center dark:text-white text-black font-bold'>Make Payments</h1>
+          <DropdownMenuTrigger className='border-none'>
+            <button className='bg-green-600 dark:bg-green-900 hover:bg-green-500 dark:hover:bg-green-800 md:h-12 p-2 rounded-lg'>
+              <h1 className='text-center dark:text-white text-black font-bold md:text-[14px] text-[11px]'>Make Payments</h1>
             </button>
           </DropdownMenuTrigger>
           <DropdownMenuContent align='end' className='dark:bg-zinc-900 p-4 flex flex-col gap-2'>
@@ -65,51 +66,45 @@ function TenantRent() {
             </Link>
           </DropdownMenuContent>
         </DropdownMenu>
+      </div>
 
-
-      
-
-      <div className='flex flex-col gap-4 absolute left-20 top-16'>
-        <Card className='w-[900px] p-0 bg-zinc-300 dark:bg-zinc-900 border-none'>
+      <div className='flex flex-col gap-4 md:absolute left-20 top-16 md:ml-0 ml-12'>
+        <Card className='md:w-[900px] w-[340px] p-0 bg-zinc-300 dark:bg-zinc-900 border-none'>
           <CardHeader className=' relative top-0'>
-            <div className='flex jusitfy-between'>
-              <div className='dark:bg-zinc-800 bg-zinc-200 p-4 relative left-24 rounded-lg'>
-              <CardTitle className='text-lg p-0 dark:text-white text-center '>Outstanding</CardTitle>
-              <CardContent className='h-[30px]'>
-                <div className='text-2xl font-bold p-0 text-orange-500 text-center'>
-                Ksh. 10000
-                </div>
-              </CardContent>
-              </div>
-              <div className='dark:bg-zinc-800 bg-zinc-200 rounded-lg p-4 relative left-[400px]'>
-                <CardTitle className='text-lg p-0 dark:text-white text-center '>Total spend</CardTitle>
-                <CardContent className='h-[30px]'>
-                  <div className='text-2xl font-bold p-0 text-orange-500 text-center'>
-                  Ksh. 100,3000
+            <div className='flex gap-4 jusitfy-between'>
+              <div className='dark:bg-zinc-800 bg-zinc-200 md:p-4 p-3 relative md:left-24 rounded-lg'>
+                <CardTitle className='md:text-lg text-sm p-0 dark:text-white text-center '>Outstanding</CardTitle>
+                <CardContent className='md:h-[30px] h-[13px]'>
+                  <div className='md:text-2xl font-bold p-0 text-orange-500 text-center'>
+                  Ksh. 10000
                   </div>
                 </CardContent>
               </div>
-              
-              
+              <div className='dark:bg-zinc-800 bg-zinc-200 rounded-lg md:p-4 p-3 relative md:left-[400px]'>
+                <CardTitle className='md:text-lg text-sm p-0 dark:text-white text-center '>Total spend</CardTitle>
+                <CardContent className='md:h-[30px] h-[13px]'>
+                  <div className='md:text-2xl text-sm font-bold p-0 text-orange-500 text-center'>
+                  Ksh. 100,3000
+                  </div>
+                </CardContent>
+              </div>              
             </div>
-            
           </CardHeader>
         </Card>
-
       </div>
         
-      <div className='absolute top-[200px] left-[70px] w-[900px] dark:bg-zinc-800 bg-zinc-300 rounded-lg mb-4'>
-        <CardHeader>
-          <CardTitle className='text-lg p-0'>Recent Transactions</CardTitle>
+      <div className='md:absolute top-[200px] left-[70px] md:ml-0 ml-12 md:w-[900px] w-[340px] dark:bg-zinc-800 bg-zinc-300 rounded-lg md:mb-4'>
+        <CardHeader className='md:p-4 p-3'>
+          <CardTitle className='md:text-lg text-sm'>Recent Transactions</CardTitle>
         </CardHeader>
-        <Table className=' '>
+        <Table className='md:text-[12.5px] text-[10px]'>
           <TableHeader className=''>
             <TableRow className='text-center hover:bg-transparent'>
               <TableHead>Status</TableHead>
               <TableHead>Due Date</TableHead>
-              <TableHead>Category</TableHead>
+              <TableHead className='md:block hidden'>Category</TableHead>
               <TableHead>Property</TableHead>
-              <TableHead>Contact</TableHead>
+              <TableHead className='md:block hidden'>Contact</TableHead>
               <TableHead>Total&Paid</TableHead>
             </TableRow>
           </TableHeader>
@@ -117,19 +112,19 @@ function TenantRent() {
             <TableRow className='hover:bg-transparent'>
               <TableCell>
                 <Badge variant='outline' className='bg-green-500 border-none'>
-                <div>Paid</div>
+                <div className='text-[10px]'>Paid</div>
                 </Badge>
               </TableCell>
               <TableCell>
                 <div>31/12/2024</div>
               </TableCell>
-              <TableCell>
+              <TableCell className='md:block hidden'>
                 <div>Open</div>
               </TableCell>
               <TableCell>
                 <div>Fine House</div>
               </TableCell>
-              <TableCell>
+              <TableCell className='md:block hidden'>
                 <div>finehouse.com</div>
               </TableCell>
               <TableCell>
@@ -139,19 +134,19 @@ function TenantRent() {
             <TableRow className='hover:bg-transparent'>
               <TableCell>
                 <Badge variant='outline' className='bg-zinc-900 border-none'>
-                  <div className='text-white'>Open</div>
+                  <div className='text-white text-[10px]'>Open</div>
                 </Badge>
               </TableCell>
               <TableCell>
                 <div>31/12/2024</div>
               </TableCell>
-              <TableCell>
+              <TableCell className='md:block hidden'>
                 <div>Open</div>
               </TableCell>
               <TableCell>
                 <div>Fine House</div>
               </TableCell>
-              <TableCell>
+              <TableCell className='md:block hidden'>
                 <div>finehouse.com</div>
               </TableCell>
               <TableCell>
@@ -161,19 +156,19 @@ function TenantRent() {
             <TableRow className='hover:bg-transparent'>
               <TableCell>
                 <Badge variant='outline' className='bg-white border-none'>
-                <div className='text-black'>Partial</div>
+                <div className='text-black text-[10px]'>Partial</div>
                 </Badge>
               </TableCell>
               <TableCell>
                 <div>31/12/2024</div>
               </TableCell>
-              <TableCell>
+              <TableCell className='md:block hidden'>
                 <div>Open</div>
               </TableCell>
               <TableCell>
                 <div>Fine House</div>
               </TableCell>
-              <TableCell>
+              <TableCell className='md:block hidden'>
                 <div>finehouse.com</div>
               </TableCell>
               <TableCell>
@@ -183,19 +178,19 @@ function TenantRent() {
             <TableRow className='hover:bg-transparent'>
               <TableCell>
                 <Badge variant='outline' className='bg-lime-600 border-none'>
-                  <div className='text-white'>Refund</div>
+                  <div className='text-white text-[10px]'>Refund</div>
                 </Badge>
               </TableCell>
               <TableCell>
                 <div>31/12/2024</div>
               </TableCell>
-              <TableCell>
+              <TableCell className='md:block hidden'>
                 <div>Open</div>
               </TableCell>
               <TableCell>
                 <div>Fine House</div>
               </TableCell>
-              <TableCell>
+              <TableCell className='md:block hidden'>
                 <div>finehouse.com</div>
               </TableCell>
               <TableCell>
@@ -205,19 +200,19 @@ function TenantRent() {
             <TableRow className='hover:bg-transparent'>
               <TableCell>
                 <Badge variant='outline' className='bg-blue-700 border-none'>
-                  <div className='text-white'>Pending</div>
+                  <div className='text-white text-[10px]'>Pending</div>
                 </Badge>
               </TableCell>
               <TableCell>
                 <div>31/12/2024</div>
               </TableCell>
-              <TableCell>
+              <TableCell className='md:block hidden'>
                 <div>Open</div>
               </TableCell>
               <TableCell>
                 <div>Fine House</div>
               </TableCell>
-              <TableCell>
+              <TableCell className='md:block hidden'>
                 <div>finehouse.com</div>
               </TableCell>
               <TableCell>
@@ -227,19 +222,19 @@ function TenantRent() {
             <TableRow className='hover:bg-transparent'>
               <TableCell>
                 <Badge variant='outline' className='bg-red-500 border-none'>
-                  <div>Void</div>
+                  <div className='text-[10px]'>Void</div>
                 </Badge>
               </TableCell>
               <TableCell>
                 <div>31/12/2024</div>
               </TableCell>
-              <TableCell>
+              <TableCell className='md:block hidden'>
                 <div>Open</div>
               </TableCell>
               <TableCell>
                 <div>Fine House</div>
               </TableCell>
-              <TableCell>
+              <TableCell className='md:block hidden'>
                 <div>finehouse.com</div>
               </TableCell>
               <TableCell>
@@ -253,13 +248,13 @@ function TenantRent() {
               <TableCell>
                 <div>31/12/2024</div>
               </TableCell>
-              <TableCell>
+              <TableCell className='md:block hidden'>
                 <div>Open</div>
               </TableCell>
               <TableCell>
                 <div>Fine House</div>
               </TableCell>
-              <TableCell>
+              <TableCell className='md:block hidden'>
                 <div>finehouse.com</div>
               </TableCell>
               <TableCell>
@@ -273,13 +268,13 @@ function TenantRent() {
               <TableCell>
                 <div>31/12/2024</div>
               </TableCell>
-              <TableCell>
+              <TableCell className='md:block hidden'>
                 <div>Open</div>
               </TableCell>
               <TableCell>
                 <div>Fine House</div>
               </TableCell>
-              <TableCell>
+              <TableCell className='md:block hidden'>
                 <div>finehouse.com</div>
               </TableCell>
               <TableCell>
@@ -293,13 +288,13 @@ function TenantRent() {
               <TableCell>
                 <div>31/12/2024</div>
               </TableCell>
-              <TableCell>
+              <TableCell className='md:block hidden'>
                 <div>Open</div>
               </TableCell>
               <TableCell>
                 <div>Fine House</div>
               </TableCell>
-              <TableCell>
+              <TableCell className='md:block hidden'>
                 <div>finehouse.com</div>
               </TableCell>
               <TableCell>
@@ -310,27 +305,27 @@ function TenantRent() {
         </Table>
       </div>
 
-      <Card className='absolute right-4 top-16 bg-zinc-300 dark:bg-zinc-900 w-[340px] h-[600px]'>
+      <Card className='md:absolute right-4 top-16 md:ml-0 ml-12 bg-zinc-300 dark:bg-zinc-900 w-[340px] h-[600px]'>
           <CardHeader className='p-2'>
-            <CardTitle className='text-lg'>Receipts</CardTitle>
+            <CardTitle className='md:text-lg text-sm'>Receipts</CardTitle>
             <Separator/>
             <div className='flex items-center pt-4'>
-              <Search className='absolute left-4 h-4 w-4'/>
+              <Search className='absolute md:left-4 md:ml-0 ml-1 h-4 w-4'/>
               <Input
               type='search'
               placeholder='Search receipts...'
-              className='w-[150px] h-[30px] pl-8'
+              className='md:w-[150px] w-[100px] md:h-[30px] h-[20px] md:pl-8 pl-6 md:text-sm text-[10px]'
               />
             </div>
           </CardHeader>
           <CardContent>
             <div className='gap-4 flex flex-col pt-4'>
-              <Table className=''>
+              <Table className='md:text-[12px] text-[10px]'>
                 <TableRow className='bg-transparent hover:bg-zinc-300 bg-zinc-300 dark:bg-zinc-900'>
-                  <TableHead className='dark:text-white text-black '>ID</TableHead>
-                  <TableHead className='dark:text-white text-black text-sm'>Payment Date</TableHead>
+                  <TableHead className='dark:text-white text-black'>ID</TableHead>
+                  <TableHead className='dark:text-white text-black'>Payment Date</TableHead>
                   <TableHead className='dark:text-white text-black'>Status</TableHead>
-                  <TableHead className='dark:text-white text-black text-sm'>Download</TableHead>
+                  <TableHead className='dark:text-white text-black'>Download</TableHead>
                 </TableRow>
                 <TableBody className=' '>
                   <TableRow className='text-center hover:bg-zinc-300 dark:hover:bg-zinc-900'>
